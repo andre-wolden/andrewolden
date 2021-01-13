0df91c0d0c7eb306478301d6b4ecd21b15c5db7e
Andre Wolden <wolden.andre@gmail.com>
2021-01-13 13:48:52 +0100

    a little styling of the CV elements

--- src/Cv.elm
+++ src/Cv.elm
@@ -1,7 +1,9 @@
 module Cv exposing (..)
 
 import Data.CvEntries exposing (cvEntries)
-import Element exposing (Element, centerX, column, el, fill, paddingEach, text)
+import Element exposing (Element, centerX, column, el, explain, fill, height, paddingEach, paragraph, px, rgb, row, text, width)
+import Element.Background exposing (color)
+import Element.Border exposing (rounded)
 import Element.Font exposing (size)
 import List exposing (map)
 import Messages exposing (Msg)
@@ -10,7 +12,7 @@ import ViewTypes exposing (CvEntry, ProjectDescription)
 
 cv : Element Msg
 cv =
-    column [ Element.width fill, paddingEach { top = 0, right = 0, bottom = 160, left = 0 } ]
+    column [ width fill, paddingEach { top = 0, right = 0, bottom = 160, left = 0 } ]
         ([ el [ centerX, size 24, paddingEach { top = 0, right = 0, bottom = 24, left = 0 } ] (text "CV")
          ]
             ++ List.map cvEntryView cvEntries
@@ -19,14 +21,19 @@ cv =
 
 cvEntryBodyView : ProjectDescription -> Element Msg
 cvEntryBodyView projectDescription =
-    Element.text "her kommer beskrivelse av prosjektet"
+    text "her kommer beskrivelse av prosjektet"
 
 
 cvEntryView : CvEntry -> Element Msg
 cvEntryView cvEntry =
-    column [ Element.width fill ]
-        [ Element.paragraph [ Element.width fill ] [ Element.text cvEntry.year ]
-        , Element.paragraph [ Element.width fill ] [ Element.text cvEntry.title ]
-        , Element.paragraph [ Element.width fill ] [ Element.text cvEntry.subTitle ]
-        , cvEntry.body |> map cvEntryBodyView |> Element.paragraph []
+    row [ paddingEach { top = 30, right = 30, bottom = 30, left = 30 } ]
+        [ column [ width (px 40) ]
+            [ el [ color (rgb 0.37 0.67 0.86), rounded 10, width (px 20), height (px 20) ] (text " ")
+            ]
+        , column [ width fill ]
+            [ paragraph [ width fill ] [ text cvEntry.year ]
+            , paragraph [ width fill ] [ text cvEntry.title ]
+            , paragraph [ width fill ] [ text cvEntry.subTitle ]
+            , cvEntry.body |> map cvEntryBodyView |> paragraph []
+            ]
         ]
