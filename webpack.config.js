1fe1809e21ecd05561c3e92f88e629f0fe71631d
Andre Wolden <wolden.andre@gmail.com>
2021-03-28 22:18:26 +0200

    set public path

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ var common = {
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, "dist"),
-        publicPath: "/",
+        publicPath: "/andrewolden",
         // FIXME webpack -p automatically adds hash when building for production
         filename: MODE === "production" ? "[name]-[hash].js" : "index.js"
     },
